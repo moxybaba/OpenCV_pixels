e3a255b05660a0dba59a136da9fa21ae155cfa57
moxybaba <mokshpanicker.7@gmal.com>
2025-04-05 12:29:47 +0530

    this is  a commit messege

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<iosteam>
+using namespace std;
+int main(){
+     int c,a,b;
+     cout<<"enter 2 no.s";
+     cin>>a>>b;
+     c=a+b;
+     cout<<"The sum of 2 no.s is "<<c<<endl;
+     return 0;
+}
